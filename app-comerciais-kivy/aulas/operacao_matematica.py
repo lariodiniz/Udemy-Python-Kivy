9955d94baacc1be5b89d99b4951dd6abaa90b87f
LarioDiniz <lario.diniz@palo.com.br>
2018-07-13 12:46:00 -0300

    Seção7 - Aula29

---
+++ app-comerciais-kivy/aulas/operacao_matematica.py
@@ -0,0 +1,31 @@
+#print(10+10)
+#print(10(50.50))
+'''
+print(10-10)
+print(1000-80)
+
+print(10/5)
+print(10/6)
+print(10//6) # devolve a divisão sem os pontos flutuantes
+
+print(10*800)
+print(55*5)
+
+#Módulo de Divisão
+
+6 % 2 #Resultado da divisão entre dois numeros
+
+print(3 % 2)
+print(4 % 2)
+print(5 % 2)
+print(7 % 3.1)
+'''
+
+num1 = float(input("Digite um numero: "))
+num2 = float(input ("Digite outro numero: "))
+
+divisao = num1 / num2
+resto = num1 % num2
+print()
+print(num1, " dividido por ", num2, " é igual a: ", divisao)
+print("o resto da divisão entre ", num1, " e ", num2, " é igual a: ", resto)
