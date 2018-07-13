b36f3246009f9a6c2286132e490def11e1b2dc05
LarioDiniz <lario.diniz@palo.com.br>
2018-07-13 18:43:29 -0300

    Seção 9 - Aula 49
    Seção 9 - Aula 49

---
+++ app-comerciais-kivy/aulas/blocos_de_instrucoes.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+num1 = int(input("Digite um numero: "))
+
+if(num1 > 10):
+    print("O valor é maior do que 10!")
+    if (num1 <= 15):
+        print("O valor é maior do que 10, mas menor do que 15!")
+    else:
+        if(num1 <= 50):
+            print("O valor é maior do que 10, mas menor do que 50!")
+        else:
+            print("O valor é maior do que 50!")
+else:
+    if(num1 > 5):
+        print("O número é menor do que 10 mas maior do que 5!")
+        if(num1 == 7):
+            print("O valor é igual a 7.")
+    else:
+        print("O valor é menor do que 5.")
