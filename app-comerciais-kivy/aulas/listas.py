70479ac587f4e9d5180328e6228cd27106d1fc60
LarioDiniz <lario.diniz@palo.com.br>
2018-07-16 11:25:10 -0300

    Seção 12 - Aula 68

--- app-comerciais-kivy/aulas/listas.py
+++ app-comerciais-kivy/aulas/listas.py
@@ -11,3 +11,18 @@ print(lista)
 
 lista = list(("eXcript",))
 print(lista)
+
+#uma lista pode conter elementos de tipos diferentes
+lista = ['a', 3]
+
+#uma lista pode conter outras listas
+lista = [['a','b','c'],[5,8,2],[]]
+print(lista[0][1])
+print(lista[1][1])
+
+#Função que retorna o tamanho de uma lista
+print("o tamanho da lista é ", len(lista))
+print("a palavra Teste tem", len("Teste"), "letras.")
+
+#o ultimo elemento de uma lista é sempre acessado com o indice -1
+print(lista[0][-1])
