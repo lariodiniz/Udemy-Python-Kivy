b4d082d3fbcc584c29e7b8c42d27660cff44b3d9
LarioDiniz <lario.diniz@palo.com.br>
2018-07-16 11:19:32 -0300

    Seção 12 - Aula 67

---
+++ app-comerciais-kivy/aulas/listas.py
@@ -0,0 +1,13 @@
+#uma lista em python é uma estrutura separada com virgulas e delimitado por colchetes
+lista = [1,2,8,5,15,3,6,8]
+
+print(lista)
+print(lista[0])
+
+l = ['a', 'b', 'c', 'd', 'e']
+
+lista = list("eXcript")
+print(lista)
+
+lista = list(("eXcript",))
+print(lista)
