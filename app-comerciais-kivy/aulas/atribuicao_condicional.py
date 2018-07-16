0107a802a6b32e53d8af9c519e34ffe98a9ea308
LarioDiniz <lario.diniz@palo.com.br>
2018-07-16 09:42:41 -0300

    Seção 10 - Aula 55

---
+++ app-comerciais-kivy/aulas/atribuicao_condicional.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8
+"""
+x = 10
+
+texto = "sim" if x == 10 else "não"
+print(texto)
+
+"""
+
+num1 = int(input("Digite um numero: "))
+
+s = "par" if num1 % 2 == 0 else "impar"
+
+print("O numero digitado é", s)
+
