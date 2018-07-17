fde8fd138dd0babbb80ab32f0c87ad940a067420
LarioDiniz <lario.diniz@palo.com.br>
2018-07-17 12:52:02 -0300

    Seção 15 - Aula 99

--- app-comerciais-kivy/aulas/funcao.py
+++ app-comerciais-kivy/aulas/funcao.py
@@ -55,7 +55,82 @@ def argumentos(*args, **kwargs):
     print(args)
     print(kwargs)
 
+"""
 lista_de_argumentos(1,2,3,4,5,6)
 lista_de_argumentos_associativos(a=1,b=2,c=3,d=4)
 
 argumentos(1,2,3,4,5,6, a=1,b=2,c=3,d=4)
+"""
+
+#desapacotamento
+def pessoa(nome, sobrenome, idade):
+    print(nome)
+    print(sobrenome)
+    print(idade)
+
+"""
+tupla = ("eXcript", "Brasil", 20)
+pessoa(*tupla)
+
+
+d = {"nome": "eXcript", "idade":20, "sobrenome": "Brasil"} #o nome da chave precisa ser o mesmo nome do argumento da função
+pessoa(**d)
+
+
+lista = [11,10,12]
+
+def funca(a,b,c):
+    print(a)
+    print(b)
+    print(c)
+
+funca(*lista)
+
+"""
+
+# aninhamento de funções
+
+def funcao():
+    print("func")
+
+    def func_interna():
+        print("Função interna")
+
+    func_interna()
+
+#Intrução NONLOCAL
+
+def func1():
+
+    var_local = 10
+
+    def func_interna():
+        nonlocal var_local
+        var_local += 1
+        print(var_local)
+    
+    func_interna()
+
+#Intrução global
+var_global = 5
+def func2():
+    
+    def func_interna():
+        global var_global # Se você quiser alterar um valor de uma variavel global  ou declarar uma variavel global, é necessario usar a palavra reservada global.
+        var_global += 1
+        print(var_global)
+    
+    func_interna()
+
+#Bloco de codigo vazio
+
+def func3():
+    pass
+
+if (True):
+    pass
+
+#Escopo global e local
+
+globals() #retorna uma lista com toda as variaveis globais
+locals() # retorna uma lista com todas as variaveis locais
