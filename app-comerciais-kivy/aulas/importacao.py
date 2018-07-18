6a1d934689aa7da599cca16910d209d81dd18268
LarioDiniz <lario.diniz@palo.com.br>
2018-07-18 11:36:42 -0300

    Seção 16 - aula 105

---
+++ app-comerciais-kivy/aulas/importacao.py
@@ -0,0 +1,13 @@
+# Importando um modulo: import ferramenta
+import math
+
+#importa o modulo e coloca na nossa tabela de simbolos (melhora performance)  : from modulo import simb1, sim1
+
+#renomeando modulo: impor modulo as m
+#renomeando simbolo: from modulo import simb1 as s1, simb2 as simb2 
+
+e = math.e
+pi = math.pi
+
+print(e)
+print(pi)
